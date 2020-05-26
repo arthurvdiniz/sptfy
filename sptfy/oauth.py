1017a7b58a49fb115afd3debd1a193848625e007
Eduardo Macedo <eduzemacedo@hotmail.com>
2020-05-26 00:00:00 -0300

    Finished ClientCredentials object, started client credentials flow

--- sptfy/oauth.py
+++ sptfy/oauth.py
@@ -1,5 +1,14 @@
+"""
+    Module responsible for taking care of the OAuth authentication flow
+    that are necessary to access the Spotify WEB API endpoints.
+"""
 import os
-from typing import Optional, List
+import base64
+from typing import Optional, List, Dict
+from requests import Session
+
+
+OAUTH_TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token'
 
 
 class ClientCredentials:
@@ -8,19 +17,75 @@ class ClientCredentials:
     """
     client_id: str
     client_secret: str
-    redirect_uri: str
+    redirect_uri: Optional[str] = None
     state: Optional[str] = None
     scope: List[str] = []
 
-    def __init__(self, client_id: str, client_secret: str, redirect_uri: str):
+    def __init__(
+            self,
+            client_id: str,
+            client_secret: str,
+            redirect_uri: str = None,
+            state: str = None,
+            scope: List[str] = None,
+        ):
         self.client_id = client_id
         self.client_secret = client_secret
-        self.redirect_uri = redirect_uri
+        if redirect_uri:
+            self.redirect_uri = redirect_uri
+        if scope:
+            self.scope = scope
+        if state:
+            self.state = state
 
     @staticmethod
-    def from_env_variables():
-        client_id = os.environ['SPTFY_CLIENT_ID']
-        client_secret = os.environ['SPTFY_CLIENT_SECRET']
-        redirect_uri = os.environ['SPTFY_REDIRECT_URI']
+    def from_env_variables(
+            client_id: Optional[str] = None,
+            client_secret: Optional[str] = None,
+            redirect_uri: Optional[str] = None
+        ):
+        client_id = client_id or os.environ['SPTFY_CLIENT_ID']
+        client_secret = client_secret or os.environ['SPTFY_CLIENT_SECRET']
+        redirect_uri = redirect_uri or os.getenv('SPTFY_REDIRECT_URI')
 
         return ClientCredentials(client_id, client_secret, redirect_uri)
+
+    def _authentication_header(self) -> Dict[str, str]:
+        encoded = f'{self.client_id}:{self.client_secret}'.encode('ascii')
+        base64_code = base64.b64encode(encoded)
+        
+        return {'Authentication': f"Basic {base64_code.decode('ascii')}"}
+
+
+
+class ClientCredentialsFlow:
+    """
+        Manages getting an access token using the Spotify API client credentials flow.
+        This flow is used when the client is not representing an user
+    """
+    credentials: ClientCredentials
+    _session: Session
+
+    def __init__(self, credentials: Optional[ClientCredentials] = None, session: Optional[Session] = None):
+
+        if credentials:
+            self.credentials = credentials
+        else:
+            self.credentials = ClientCredentials.from_env_variables()
+
+        if session:
+            self._session = session
+        else:
+            self._session = Session()
+
+    def _request_access_token(self):
+        payload = {'grant_type': 'client_credentials'}
+        auth_header = self.credentials._authentication_header()
+
+        response = self._session.post(
+            OAUTH_TOKEN_ENDPOINT,
+            headers=auth_header,
+            data=payload,
+        )
+
+        return response.json() 
--- tests/test_oauth.py
+++ tests/test_oauth.py
@@ -1,5 +1,32 @@
+import base64
+import json
 import pytest
+import requests
+import responses
+from unittest.mock import patch
+
 import sptfy.oauth as oauth
+
+
+CRED_INIT_VARS = [
+        (
+            {'client_id': 'client-id-123456', 'client_secret': 'secret-key-456789'},
+            {'scope': [], 'state':None, 'redirect_uri': None}
+        ),
+        (
+            {'client_id': 'client-id-123456', 'client_secret':'secret-key-456789', 'redirect_uri': 'http://localhost:4200/'},
+            {'scope': [], 'state': None}
+        ),
+        (
+            {'client_id': 'client-id-123456', 'client_secret': 'secret-key-456789', 'redirect_uri': 'http://localhost:4200/', 'state': 'random-state-string'},
+            {'scope': []}
+        ),
+        (
+            {'client_id': 'client-id-123456', 'client_secret': 'secret-key-456789', 'redirect_uri': 'http://localhost:4200/', 'state': 'random-state-string', 'scope': ['foo', 'bar']},
+            {},
+        )
+]
+
 
 @pytest.fixture()
 def sptfy_environment(monkeypatch):
@@ -12,6 +39,12 @@ def sptfy_environment(monkeypatch):
     monkeypatch.setenv('SPTFY_REDIRECT_URI', redirect_uri)
 
     return client_id, client_secret, redirect_uri
+
+
+def authentication_header(client_id, client_secret):
+    credentials_header = f'{client_id}:{client_secret}'.encode(encoding='ascii')
+    authentication_header = base64.b64encode(credentials_header)
+    return {'Authentication': f"Basic {authentication_header.decode('ascii')}"}
 
 
 def test_load_credentials_from_env(sptfy_environment):
@@ -35,12 +68,88 @@ def test_load_credentials_without_env_should_throw():
         oauth.ClientCredentials.from_env_variables()
 
 
+@pytest.mark.parametrize("test_input,defaults", CRED_INIT_VARS)
+def test_credentials_initialization(test_input, defaults):
+
+    # WHEN: Initialized with custom variables 
+    credentials = oauth.ClientCredentials(**test_input)
+
+    # THEN: Credentials should be initialized with default values
+    for key, value in defaults.items():
+        assert getattr(credentials, key) == value
+
+
 def test_credentials_initialization_has_default_variables(sptfy_environment):
     # GIVEN: The credentials stored in program variables
     client_id, client_secret, redirect_uri = sptfy_environment
 
-    credentials = oauth.ClientCredentials(client_id, client_secret, redirect_uri)
+    # WHEN: Initialized only with client ID and client secret
+    credentials = oauth.ClientCredentials(client_id, client_secret)
 
     # THEN: It should have an empty scope and no state
+    assert credentials.redirect_uri is None
     assert credentials.scope == []
-    assert credentials.state == None
+    assert credentials.state is None
+
+
+def test_credentials_get_auth_header(sptfy_environment):
+    # GIVEN: A client id and a client_secret
+    client_id, client_secret, _ = sptfy_environment
+    credentials = oauth.ClientCredentials(client_id, client_secret)
+    auth_header = authentication_header(credentials.client_id, credentials.client_secret)
+    
+    # WHEN: authentication_header is called
+    header = credentials._authentication_header()
+
+    # THEN: The header should be a dict containing a base64 encoded
+    # string that represents the client_id and client_secret
+    assert header['Authentication'] == auth_header['Authentication']
+
+
+def test_oauth_client_credentials_flow_from_env(sptfy_environment):
+    # GIVEN: Environment variables necessary for client credentials flow
+    client_id, client_secret, redirect_uri = sptfy_environment
+
+    # WHEN: client credentials flow is initialized with no parameters
+    credentials_flow = oauth.ClientCredentialsFlow()
+
+    # THEN: Credentials variables should be loaded from env variables
+    assert credentials_flow.credentials.client_id == client_id
+    assert credentials_flow.credentials.client_secret == client_secret
+    # redirect_uri is not necessary for client credentials flow, but
+    # it's still loaded if the variable is defined
+    assert credentials_flow.credentials.redirect_uri == redirect_uri
+
+
+@responses.activate
+def test_oauth_client_credentials_request_token(sptfy_environment):
+    # GIVEN: ClientCredentialsFlow object initialized with the client credentials
+    client_id, client_secret, _ = sptfy_environment
+    credentials = oauth.ClientCredentials(client_id, client_secret)
+    auth_header = authentication_header(credentials.client_id, credentials.client_secret)
+    credentials_flow = oauth.ClientCredentialsFlow(credentials)
+
+    some_token = {
+        'access_token':  'random-string-from-server',
+        'client_id': client_id,
+        'scope': 'foo bar',
+        'expires_in': 3600,
+    }
+
+    responses.add(
+        responses.POST,
+        oauth.OAUTH_TOKEN_ENDPOINT,
+        json=some_token,
+        status=200
+    )
+
+    # WHEN: request_access_token is called
+    token = credentials_flow._request_access_token()
+
+    # THEN: The response should be the token string
+    assert token == some_token
+    token_request = responses.calls[0].request
+    # the header variables contains the Authentication header
+    assert token_request.headers['Authentication'] == auth_header['Authentication']
+    # The payload should indentify the grant_type
+    assert token_request.body == 'grant_type=client_credentials'
